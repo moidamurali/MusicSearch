01aaf5f60330a5bb9632160110537e7814b8c2e4
muralimohanarao_r <muralimohanarao.ramarao@photoninfotech.net>
2019-12-17 20:33:22 +0530

    Music Search with Cache Image

--- app/src/main/java/com/zify/musicsearch/utils/Constants.java
+++ app/src/main/java/com/zify/musicsearch/utils/Constants.java
@@ -1,8 +1,60 @@
 package com.zify.musicsearch.utils;
 
+import android.content.Context;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class Constants {
 
     public static final String ARTIST_SEARCH_ENDPINT_URL = "http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=";
     private static final String ARTIST_INFO_ENDPINT_URL = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=";
     private static final String API_KEY = "fa2e62987b8c372e16daa60331164d12";
+
+    private void writeToFile(String data,Context context, String fileName) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput(fileName, Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
+
+    private String readFromFile(Context context, String fileName) {
+//"config.txt"
+        String ret = "";
+
+        try {
+            InputStream inputStream = context.openFileInput(fileName);
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+
+        return ret;
+    }
 }
